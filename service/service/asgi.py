1ea7f184de0dd5bb7de819501880dea72d2df75f
TMN <super.rikunen@gmail.com>
2023-11-28 23:25:02 +0300

    Django setup

--- service/service/asgi.py
+++ service/service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for image_resize project.
+ASGI config for service project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'image_resize.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service.settings')
 
 application = get_asgi_application()
