a0380b10302587adb859a97ff53d58aaac0e8d33
TMN <super.rikunen@gmail.com>
2023-11-28 23:47:08 +0300

    Models setup

--- service/image_resize/models.py
+++ service/image_resize/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    name = models.CharField(max_length=100)
+    height_before = models.IntegerField()
+    width_before = models.IntegerField()
+    height_after = models.IntegerField()
+    width_after = models.IntegerField()
+
+    def __str__(self):
+        return f"Image: {self.name}"
